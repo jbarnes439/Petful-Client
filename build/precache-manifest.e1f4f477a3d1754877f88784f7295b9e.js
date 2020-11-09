d8e913c33f4131e5fadeaa152b36f03020235dc4
Josh Barnes <jbarnes439@gmail.com>
2020-11-08 20:36:47 -0700

    prepared for deployment

---
+++ build/precache-manifest.e1f4f477a3d1754877f88784f7295b9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f52ebe57d68629121d583647ecc1356b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0eeda3a018204ac4530a",
+    "url": "/static/css/main.eac004d7.chunk.css"
+  },
+  {
+    "revision": "3aec2885913dd5abab21",
+    "url": "/static/js/2.c0a7a6bb.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.c0a7a6bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eeda3a018204ac4530a",
+    "url": "/static/js/main.b408d406.chunk.js"
+  },
+  {
+    "revision": "72c5965cbfb3ac6a55d7",
+    "url": "/static/js/runtime-main.40d8ffab.js"
+  },
+  {
+    "revision": "057f83dea88e0197d05f702330ab2ed7",
+    "url": "/static/media/cats-and-dogs.057f83de.jpg"
+  }
+]);
